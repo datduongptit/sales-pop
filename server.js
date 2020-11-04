bb873b0404d899965d233c91cca81016bfca46a8
datduongptit <datduongptit@gmail.com>
2020-11-04 15:29:52 +0700

    deploy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
+const path = require('path');
 const app = express();
 
 //Connect database
@@ -9,12 +9,21 @@ connectDB();
 // init middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Running'));
 
 //define routes
 app.use('/api/notificationSetting', require('./routes/apis/notificationSetting'));
 app.use('/api/manualSale', require('./routes/apis/manualSale'));
 app.use('/api/realtimeSetting', require('./routes/apis/realtimeSetting'));
+
+// server static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 const PORT = process.env.PORT || 5000;
 
